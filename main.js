2878c0944e2c0fdaa6be4fa87093d57964f2c73a
Veer16 <veer.mailer@gmail.com>
2019-07-04 19:43:55 +1200

    implemented the local storage for the name and the focus

--- main.js
+++ main.js
@@ -52,6 +52,56 @@ function setBgGreet() {
         document.body.style.color = "white";
     }
 }
+
+// Get Name 
+function getName() {
+    if (localStorage.getItem('name') === null) {
+        name.textContent = "[Enter Name]";
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
+}
+// Set Name
+function setName(e) {
+    // Check the key press
+    if (e.type == 'keypress') {
+        // check the keytype
+        if (e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('name', e.target.innerHTML);
+            name.blur();
+        }
+    } else {
+        // Implementing Blur
+        localStorage.setItem('name', e.target.innerHTML);
+    }
+}
+// Get Focus 
+function getFocus() {
+    if (localStorage.getItem('focus') === null) {
+        focus.textContent = "[Enter Focus]";
+    } else {
+        name.textContent = localStorage.getItem('focus');
+    }
+}
+// Set Focus
+
+function setFocus(e) {
+    if (e.type = 'keypress') {
+        if (e.which == 13 || e.keyCode == 13) {
+            localStorage.setItem('focus', e.target.innerHTML);
+            focus.blur();
+        }
+    } else {
+        localStorage.setItem('focus', e.target.innerHTML);
+    }
+}
+//Event Listeners for the name and the focus
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
 //Run Functions
 showTime();
 setBgGreet();
+getName();
+getFocus();
